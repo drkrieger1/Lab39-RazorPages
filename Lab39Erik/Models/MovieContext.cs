26932176bfdcb676526d99d348b833e8e19827e3
drkrieger <eplyushko@gmail.com>
2017-11-06 10:11:33 -0800

    movie model created and the movie context

---
+++ Lab39Erik/Models/MovieContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab39Erik.Models
+{
+    public class MovieConext : DbContext 
+    {
+        public MovieContext(DbContextOptions<MovieContext> options)
+                : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
