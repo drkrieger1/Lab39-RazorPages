b3a49ae7ec0311a5ec700861866cc62f048f6b3b
drkrieger <eplyushko@gmail.com>
2017-11-06 21:03:04 -0800

    adding search is joke, tutorial code does not work

--- Lab39Erik/Pages/Index.cshtml.cs
+++ Lab39Erik/Pages/Index.cshtml.cs
@@ -4,14 +4,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Lab39Erik.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Lab39Erik.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private MovieContext _context;
 
-        }
+        public IndexModel(MovieContext context) => _context = context;
+
+        //public async Task OnGetAsync(string searchString)
+        //{
+        //    var movies = from m in _context.Movie
+        //                 select m;
+
+        //    if (!String.IsNullOrEmpty(searchString))
+        //    {
+        //        movies = movies.Where(s => s.Title.Contains(searchString));
+        //    }
+
+        //     return movies
+        //    //movies = await movies.ToListAsync();
+        //}
+
     }
 }
